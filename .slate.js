78ba13eae8aadc4d6328a3be7183ef27b39946cf
James Chen <ashchan@gmail.com>
2014-02-25 16:53:28 +0900

    Remove before adding my own config

--- .slate.js
+++ .slate.js
@@ -15,94 +15,19 @@ S.cfga({
 });
 
 // Monitors
-var monTboltL = "0";
-var monTboltR = "2";
+var monU2311H = "0";
 var monLaptop = "1440x900";
-
-// Operations
-var lapChat = S.op("corner", {
-  "screen" : monLaptop,
-  "direction" : "top-left",
-  "width" : "screenSizeX/4",
-  "height" : "screenSizeY"
-});
-var lapMain = lapChat.dup({ "direction" : "top-right", "width" : "3*screenSizeX/4" });
-var tboltLFull = S.op("move", {
-  "screen" : monTboltL,
-  "x" : "screenOriginX",
-  "y" : "screenOriginY",
-  "width" : "screenSizeX",
-  "height" : "screenSizeY"
-});
-var tboltLLeft = tboltLFull.dup({ "width" : "screenSizeX/3" });
-var tboltLMid = tboltLLeft.dup({ "x" : "screenOriginX+screenSizeX/3" });
-var tboltLRight = tboltLLeft.dup({ "x" : "screenOriginX+(screenSizeX*2/3)" });
-var tboltLLeftTop = tboltLLeft.dup({ "height" : "screenSizeY/2" });
-var tboltLLeftBot = tboltLLeftTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var tboltLMidTop = tboltLMid.dup({ "height" : "screenSizeY/2" });
-var tboltLMidBot = tboltLMidTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var tboltLRightTop = tboltLRight.dup({ "height" : "screenSizeY/2" });
-var tboltLRightBot = tboltLRightTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var tboltRFull = S.op("move", {
-  "screen" : monTboltR,
-  "x" : "screenOriginX",
-  "y" : "screenOriginY",
-  "width" : "screenSizeX",
-  "height" : "screenSizeY"
-});
-var tboltRLeft = tboltRFull.dup({ "width" : "screenSizeX/3" });
-var tboltRMid = tboltRLeft.dup({ "x" : "screenOriginX+screenSizeX/3" });
-var tboltRRight = tboltRLeft.dup({ "x" : "screenOriginX+(screenSizeX*2/3)" });
-var tboltRLeftTop = tboltRLeft.dup({ "height" : "screenSizeY/2" });
-var tboltRLeftBot = tboltRLeftTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var tboltRMidTop = tboltRMid.dup({ "height" : "screenSizeY/2" });
-var tboltRMidBot = tboltRMidTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var tboltRRightTop = tboltRRight.dup({ "height" : "screenSizeY/2" });
-var tboltRRightBot = tboltRRightTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-
-// common layout hashes
-var lapMainHash = {
-  "operations" : [lapMain],
-  "ignore-fail" : true,
-  "repeat" : true
-};
-var chatHash = {
-  "operations" : [lapChat, lapMain],
-  "ignore-fail" : true,
-  "title-order" : ["Buddies"],
-  "repeat-last" : true
-};
-var terminalHash = {
-  "operations" : [tboltLMidTop, tboltLMidBot, tboltRMidTop, tboltRMidBot, tboltRRightBot],
-  "sort-title" : true,
-  "repeat" : true
-};
-var genBrowserHash = function(regex) {
-  return {
-    "operations" : [function(windowObject) {
-      var title = windowObject.title();
-      if (title !== undefined && title.match(regex)) {
-        windowObject.doOperation(tboltLLeft);
-      } else {
-        windowObject.doOperation(lapMain);
-      }
-    }],
-    "ignore-fail" : true,
-    "repeat" : true
-  };
-};
 
 // 1 monitor layout
 var oneMonitorLayout = S.lay("oneMonitor", {
-  "Messages" :chatHash,
-  "Terminal" : lapMainHash,
-  "Xcode" : lapMainHash,
-  "Safari" : lapMainHash,
-  "Spotify" : lapMainHash
+  //"Messages" :chatHash,
+  //"Terminal" : lapMainHash,
+  //"Xcode" : lapMainHash,
+  //"Safari" : lapMainHash,
+  //"Spotify" : lapMainHash
 });
 
-var twoMonitorLayout = S.lay("twoMonitor", {
-  "Terminal" : terminalHash,
+var twoMonitorLayout = S.layout("twoMonitor", {
 });
 
 // Defaults
@@ -127,8 +52,8 @@ S.bnda({
   //"space:ctrl" : universalLayout,
 
   // Basic Location Bindings
-  "left:alt" : lapChat,
-  "right:alt" : lapMain,
+  //"left:alt" : lapChat,
+  //"right:alt" : lapMain,
 
   // Resize Bindings
   "right:cmd;shift" : S.op("resize", { "width" : "+5%", "height" : "+0" }),
@@ -161,26 +86,7 @@ S.bnda({
   "up:ctrl;alt;cmd" : S.op("throw", { "screen" : "up", "width" : "screenSizeX", "height" : "screenSizeY" }),
   "down:ctrl;alt;cmd" : S.op("throw", { "screen" : "down", "width" : "screenSizeX", "height" : "screenSizeY" }),
 
-  // Focus Bindings
-  //"l:cmd" : S.op("focus", { "direction" : "right" }),
-  //"h:cmd" : S.op("focus", { "direction" : "left" }),
-  //"k:cmd" : S.op("focus", { "direction" : "up" }),
-  //"j:cmd" : S.op("focus", { "direction" : "down" }),
-  //"k:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-  //"j:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-  //"right:cmd" : S.op("focus", { "direction" : "right" }),
-  //"left:cmd" : S.op("focus", { "direction" : "left" }),
-  //"up:cmd" : S.op("focus", { "direction" : "up" }),
-  //"down:cmd" : S.op("focus", { "direction" : "down" }),
-  //"up:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-  //"down:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-
-  // Window Hints
+  // Hint and grid
   "esc:cmd" : S.op("hint"),
-
-  // Switch currently doesn't work well so I'm commenting it out until I fix it.
-  //"tab:cmd" : S.op("switch"),
-
-  // Grid
   "esc:ctrl" : S.op("grid")
 });
