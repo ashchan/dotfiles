c5395baa9c9e29096569968ab5a9377a86456f07
James Chen <ashchan@gmail.com>
2014-02-25 17:50:24 +0900

    This .slate.js is working for common tasks

--- .slate.js
+++ .slate.js
@@ -1,42 +1,28 @@
-// Configs
-S.cfga({
-  "defaultToCurrentScreen" : true,
-  "windowHintsShowIcons" : true,
-  "windowHintsIgnoreHiddenWindows" : true,
-  "windowHintsSpread" : true,
-  "windowHintsDuration" : 2.5,
-  "nudgePercentOf" : "screenSize",
-  "resizePercentOf" : "screenSize",
-  "secondsBetweenRepeat" : 0.1,
-  "secondsBeforeRepeat" : 0.3,
-  "checkDefaultsOnLoad" : true,
-  "focusCheckWidthMax" : 3000,
-  "orderScreensLeftToRight" : true
+S.configAll({
+  "defaultToCurrentScreen":         true,
+  "checkDefaultsOnLoad":            true,
+  "orderScreensLeftToRight":        true,
+  "windowHintsShowIcons":           true,
+  "windowHintsSpread":              true
 });
 
-// Monitors
-var monU2311H = "0";
-var monLaptop = "1440x900";
-
-// 1 monitor layout
-var oneMonitorLayout = S.lay("oneMonitor", {
-  //"Messages" :chatHash,
-  //"Terminal" : lapMainHash,
-  //"Xcode" : lapMainHash,
-  //"Safari" : lapMainHash,
-  //"Spotify" : lapMainHash
+var oneMonitorLayout = S.layout("oneMonitor", {
+  "Messages": {},
+  "Terminal": {},
+  "Xcode": {},
+  "Safari": {},
+  "Spotify": {},
+  "TODO Simulator": {}
 });
 
 var twoMonitorLayout = S.layout("twoMonitor", {
 });
 
-// Defaults
-S.def(2, twoMonitorLayout);
-S.def(1, oneMonitorLayout);
+S.default(2, twoMonitorLayout);
+S.default(1, oneMonitorLayout);
 
-// Layout Operations
-var twoMonitor = S.op("layout", { "name" : twoMonitorLayout });
-var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
+var twoMonitor = S.operation("layout", { "name" : twoMonitorLayout });
+var oneMonitor = S.operation("layout", { "name" : oneMonitorLayout });
 var universalLayout = function() {
   if (S.screenCount() === 2) {
     twoMonitor.run();
@@ -45,8 +31,7 @@ var universalLayout = function() {
   }
 };
 
-// Batch bind everything. Less typing.
-S.bnda({
+S.bindAll({
   // Layout Bindings
   //"padEnter:ctrl" : universalLayout,
   //"space:ctrl" : universalLayout,
@@ -55,38 +40,27 @@ S.bnda({
   //"left:alt" : lapChat,
   //"right:alt" : lapMain,
 
-  // Resize Bindings
-  "right:cmd;shift" : S.op("resize", { "width" : "+5%", "height" : "+0" }),
-  "left:cmd;shift" : S.op("resize", { "width" : "-5%", "height" : "+0" }),
-  "up:cmd:shift" : S.op("resize", { "width" : "+0", "height" : "-5%" }),
-  "down:cmd;shift" : S.op("resize", { "width" : "+0", "height" : "+5%" }),
-  //"right:alt" : S.op("resize", { "width" : "-5%", "height" : "+0", "anchor" : "bottom-right" }),
-  //"left:alt" : S.op("resize", { "width" : "+5%", "height" : "+0", "anchor" : "bottom-right" }),
-  "up:alt" : S.op("resize", { "width" : "+0", "height" : "+5%", "anchor" : "bottom-right" }),
-  "down:alt" : S.op("resize", { "width" : "+0", "height" : "-5%", "anchor" : "bottom-right" }),
+  "m:shift;cmd": S.op("move", { x: "screenOriginX", y: "screenOriginY", width: "screenSizeX", height: "screenSizeY" }),
 
-  // Push Bindings
-  "right:cmd" : S.op("push", { "direction" : "right", "style" : "bar-resize:screenSizeX/2" }),
-  "left:cmd" : S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/2" }),
-  "up:cmd" : S.op("push", { "direction" : "up", "style" : "bar-resize:screenSizeY/2" }),
-  "down:cmd" : S.op("push", { "direction" : "down", "style" : "bar-resize:screenSizeY/2" }),
+  "right:cmd": S.op("push", { "direction" : "right", "style" : "bar-resize:screenSizeX/2" }),
+  "left:cmd": S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/2" }),
+  "up:cmd": S.op("push", { "direction" : "up", "style" : "bar-resize:screenSizeY/2" }),
+  "down:cmd": S.op("push", { "direction" : "down", "style" : "bar-resize:screenSizeY/2" }),
 
-  // Nudge Bindings
-  "right:shift;alt" : S.op("nudge", { "x" : "+5%", "y" : "+0" }),
-  "left:shift;alt" : S.op("nudge", { "x" : "-5%", "y" : "+0" }),
-  "up:shift;alt" : S.op("nudge", { "x" : "+0", "y" : "-5%" }),
-  "down:shift;alt" : S.op("nudge", { "x" : "+0", "y" : "+5%" }),
+  "right:cmd;shift": S.op("resize", { "width" : "+5%", "height" : "+0" }),
+  "left:cmd;shift": S.op("resize", { "width" : "-5%", "height" : "+0" }),
+  "up:cmd:shift": S.op("resize", { "width" : "+0", "height" : "-5%" }),
+  "down:cmd;shift": S.op("resize", { "width" : "+0", "height" : "+5%" }),
+  "up:alt": S.op("resize", { "width" : "+0", "height" : "+5%", "anchor" : "bottom-right" }),
+  "down:alt": S.op("resize", { "width" : "+0", "height" : "-5%", "anchor" : "bottom-right" }),
 
-  // Throw Bindings
-  "pad1:ctrl;alt" : S.op("throw", { "screen" : "2", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "pad2:ctrl;alt" : S.op("throw", { "screen" : "1", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "pad3:ctrl;alt" : S.op("throw", { "screen" : "0", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "right:ctrl;alt;cmd" : S.op("throw", { "screen" : "right", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "left:ctrl;alt;cmd" : S.op("throw", { "screen" : "left", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "up:ctrl;alt;cmd" : S.op("throw", { "screen" : "up", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "down:ctrl;alt;cmd" : S.op("throw", { "screen" : "down", "width" : "screenSizeX", "height" : "screenSizeY" }),
+  "right:shift;alt": S.op("nudge", { "x" : "+5%", "y" : "+0" }),
+  "left:shift;alt": S.op("nudge", { "x" : "-5%", "y" : "+0" }),
+  "up:shift;alt": S.op("nudge", { "x" : "+0", "y" : "-5%" }),
+  "down:shift;alt": S.op("nudge", { "x" : "+0", "y" : "+5%" }),
 
-  // Hint and grid
-  "esc:cmd" : S.op("hint"),
-  "esc:ctrl" : S.op("grid")
+  "up:shift;cmd": S.op("throw", { "screen" : "2", "width" : "screenSizeX", "height" : "screenSizeY" }),
+
+  "esc:cmd": S.op("hint"),
+  "esc:ctrl": S.op("grid")
 });
