0b567aee4ebd5999502ae031f054611f4e6823d3
James Chen <ashchan@gmail.com>
2014-07-04 14:12:49 +0900

    Config files for hydra

---
+++ .hydra/init.lua
@@ -0,0 +1,60 @@
+require "grid"
+
+hydra.alert "Hydra loaded."
+
+pathwatcher.new(os.getenv("HOME") .. "/.dotify/.hydra/", hydra.reload):start()
+autolaunch.set(true)
+
+menu.show(function()
+    return {
+      {title = "Reload Config", fn = hydra.reload},
+      {title = "-"},
+      {title = "About", fn = hydra.showabout},
+      {title = "Quit Hydra", fn = os.exit},
+    }
+end)
+
+local mash = {"cmd", "shift"}
+
+local function opendictionary()
+    application.launchorfocus("Dictionary")
+end
+
+local function movewindow_rightside()
+  local win = window.focusedwindow()
+  local newframe = win:screen():frame_without_dock_or_menu()
+  newframe.w = newframe.w / 2
+  newframe.x = newframe.w
+  win:setframe(newframe)
+end
+
+local function movewindow_leftside()
+  local win = window.focusedwindow()
+  local newframe = win:screen():frame_without_dock_or_menu()
+  newframe.w = newframe.w / 2
+  win:setframe(newframe)
+end
+
+hotkey.bind(mash, 'D', opendictionary)
+
+hotkey.bind(mash, ';', function() ext.grid.snap(window.focusedwindow()) end)
+
+hotkey.bind(mash, '=', function() ext.grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() ext.grid.adjustwidth(-1) end)
+
+hotkey.bind(mash, 'M', ext.grid.maximize_window)
+
+hotkey.bind(mash, 'N', ext.grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', ext.grid.pushwindow_prevscreen)
+
+hotkey.bind(mash, 'J', ext.grid.pushwindow_down)
+hotkey.bind(mash, 'K', ext.grid.pushwindow_up)
+hotkey.bind(mash, 'H', movewindow_leftside)
+hotkey.bind(mash, 'L', movewindow_rightside)
+
+hotkey.bind(mash, 'O', ext.grid.resizewindow_wider)
+hotkey.bind(mash, 'I', ext.grid.resizewindow_thinner)
+
+hotkey.bind(mash, 'X', logger.show)
+hotkey.bind(mash, "R", repl.open)
+
